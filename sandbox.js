cde32bee25d7dc5d3ca11f219c8095080d74a4e9
Vilas Chavhan <vilas.chavhan@seaflux.tech>
2023-06-19 18:36:46 +0530

    playing with different types

--- sandbox.js
+++ sandbox.js
@@ -4,6 +4,13 @@ input.forEach(function (ele) { return console.log(ele); });
 //lesson 3
 var char = 'batman';
 char = 'bruce wyane';
+var batman = "bruce Wyane";
+console.log(batman, "is the batman");
+var bw = true;
+console.log(batman, "is the batman??", bw);
+//we can avoid ': type', typscript will autometically inference or defines types.
+var stringChar = 'this is string type....';
+console.log(stringChar);
 function pi(num) {
     return num * Math.PI;
 }
--- sandbox.ts
+++ sandbox.ts
@@ -8,6 +8,18 @@ input.forEach(ele => console.log(ele));
 var char = 'batman';
 char = 'bruce wyane';
 
+var batman : string = "bruce Wyane";
+console.log(batman,"is the batman"); 
+
+var bw : boolean = true;
+console.log(batman,"is the batman??",bw);
+
+//we can avoid ': type', typscript will autometically inference or defines types.
+
+var stringChar = 'this is string type....';
+console.log(stringChar);
+
+
 function pi(num : number){
     return num * Math.PI;
 }
