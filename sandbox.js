3d8fec3b2f97fffb3001964c4574d27e97a41718
Vilas Chavhan <vilas.chavhan@seaflux.tech>
2023-06-19 18:06:48 +0530

    lesson-3 : types

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,11 @@
 var input = document.querySelectorAll("input");
 console.log(input);
 input.forEach(function (ele) { return console.log(ele); });
+//lesson 3
+var char = 'batman';
+char = 'bruce wyane';
+function pi(num) {
+    return num * Math.PI;
+}
+// console.log(pi("hasd")); error
+console.log(pi(10));
--- sandbox.ts
+++ sandbox.ts
@@ -2,3 +2,15 @@ const input = document.querySelectorAll("input");
 console.log(input);
 
 input.forEach(ele => console.log(ele));
+
+//lesson 3
+
+var char = 'batman';
+char = 'bruce wyane';
+
+function pi(num : number){
+    return num * Math.PI;
+}
+
+// console.log(pi("hasd")); error
+console.log(pi(10));
